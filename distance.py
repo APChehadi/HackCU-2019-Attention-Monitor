8ca437c1e7c5deb85de85112e628fc39dfb09c4a
Sam Williams <samuel.j.williams@colorado.edu>
2019-02-24 10:24:07 -0700

    remove keys

--- distance.py
+++ distance.py
@@ -1,23 +1,10 @@
 import requests
 
 #google api key
-key = "AIzaSyDg8vHOW6g5lLOPhVex902xO4E4QPPDYbc"
+key = ""
 
 #rapidAPI key
-rapidapiKey = "27f39824a7msh9e7433c797ab87fp1f6286jsn12e98b6e9682"
-
-#RapidAPI :()
-#response = requests.post("https://GoogleMapsDistanceMatrixstefan.skliarovV1.p.rapidapi.com/getBicyclingDistanceMatrix",
-#  headers={
-#    "X-RapidAPI-Key": "2a44635751msh2243a006353b957p19f690jsn06c4fb17b7fc",
-#    "Content-Type": "application/x-www-form-urlencoded"
-#  },
-#  data={
-#  	"origins": "40.5, -100.721",
-#  	"destinations": "2414 Regent Dr, Boulder, CO 80309",
-#  	"apiKey": "AIzaSyDg8vHOW6g5lLOPhVex902xO4E4QPPDYbc"
-#  }
-#)
+rapidapiKey = ""
 
 #Google API
 def getDistance(source, dest):
