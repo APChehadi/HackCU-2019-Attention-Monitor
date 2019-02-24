75759dd8dd1ba8d3980290cf3056c9478ac6aef9
Sam Williams <samuel.j.williams@colorado.edu>
2019-02-24 10:23:31 -0700

    Add files via upload

--- distance.py
+++ distance.py
@@ -1,7 +1,10 @@
 import requests
 
 #google api key
-key = -
+key = "AIzaSyDg8vHOW6g5lLOPhVex902xO4E4QPPDYbc"
+
+#rapidAPI key
+rapidapiKey = "27f39824a7msh9e7433c797ab87fp1f6286jsn12e98b6e9682"
 
 #RapidAPI :()
 #response = requests.post("https://GoogleMapsDistanceMatrixstefan.skliarovV1.p.rapidapi.com/getBicyclingDistanceMatrix",
@@ -22,3 +25,9 @@ def getDistance(source, dest):
 
 def getGeocode(address):
 	return requests.post("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + key)
+
+def getGeolocation():
+	return requests.post("https://www.googleapis.com/geolocation/v1/geolocate?key=" + key)
+
+def getWeather(lat, lon):
+	return requests.get("https://community-open-weather-map.p.rapidapi.com/weather?lat=" + lat + "&lon=" + lon + "&units=imperial", headers={"X-RapidAPI-Key": rapidapiKey})
