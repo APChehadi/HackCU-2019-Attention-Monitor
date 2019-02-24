499b4ba0e0295404677f6baa64baf45cd6f80d84
Vikram-T <vikram.tholakapalli@gmail.com>
2019-02-24 04:21:51 -0700

    Modified BokehData.py

--- BokehData.py
+++ BokehData.py
@@ -3,15 +3,16 @@ import scipy.special
 import pandas as pd
 from random import random
 
-from bokeh.models import Button
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file, curdoc
 from bokeh.layouts import column
 from bokeh.models import Button
+from bokeh.models.widgets import Panel, Tabs
+from bokeh.io import show, output_file
 
 
 #This function creates the visualization of the Histogram
-def make_histo(title, array, x):
+def make_histo(title, array):
     #Creates the mathmatical information for the histogram
     hist, edges = np.histogram(eyeRatios, density=False, bins=10)
     p = figure(title=title, background_fill_color="#fafafa") #Creates base of graph
@@ -37,40 +38,21 @@ def make_line_plot(title, data):
     linePlot.circle(x,y, size=7)
     return linePlot
 
-
-
 eyeRatios = np.random.random_sample(30)
 rng = pd.date_range('2018-01-01', periods=30, freq='S')
 
 week = [rng, eyeRatios]
-p1 = make_histo("Eye Ratio Tracking", eyeRatios, eyeRatios)
+p1 = make_histo("Eye Ratio Tracking", eyeRatios)
 
 p2 = make_line_plot("Eye Ratio Tracking", week)
+
+
+tab1 = Panel(child=p1, title="Histogram")
+tab2= Panel(child=p2, title="Line Chart")
+
 output_file('histogram.html', title="histogram.py example")
+tabs = Tabs(tabs=[tab1,tab2])
+show(tabs)
 
-
-
-# add a text renderer to our plot (no data yet)
-r = linePlot.circle(x=[], y=[])
-
-
-ds = r.data_source
-
-# create a callback that will add a number in a random location
-def callback():
-
-    # BEST PRACTICE --- update .data in one step with a new dict
-    new_data = dict()
-    new_data['x'] = ds.data['x'] + [random()*70 + 15]
-    new_data['y'] = ds.data['y'] + [random()*70 + 15]
-    ds.data = new_data
-
-
-# add a button widget and configure with the call back
-button = Button(label="Press Me")
-button.on_click(callback)
-
-# # put the button and plot in a layout and add to the document
-curdoc().add_root(column(button, p2))
 
 
